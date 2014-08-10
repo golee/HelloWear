759576bbe7333a6a08da767025801bc8309f6cf9
golee <golee3@gmail.com>
2014-08-10 15:33:37 +0900

    Layout for Activity that function of is transfering a text message to Wear

--- mobile/src/main/java/hello/wear/hellowear/MyActivity.java
+++ mobile/src/main/java/hello/wear/hellowear/MyActivity.java
@@ -2,18 +2,62 @@ package hello.wear.hellowear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+
+    private EditText mEditText;
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        mEditText=(EditText)findViewById(R.id.editText1);
+        mTextView=(TextView)findViewById(R.id.textView1);
+        mButton=(Button)findViewById(R.id.button1);
+
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                    mTextView.setText(mEditText.getText());
+                    return true;
+                }
+                else
+                {
+                    mTextView.setText("actionId : "+actionId+" EditorInfo : "+EditorInfo.IME_ACTION_UNSPECIFIED);
+                }
+                return false;
+            }
+        });
     }
 
+
+    public void onClick(View v)
+    {
+        if (sendMessage(mTextView.getText()))
+        {
+            mTextView.setText("\""+mTextView.getText()+" \" transfer success!");
+        }
+        else
+        {
+
+        }
+    }
+    public boolean sendMessage(CharSequence s)
+    {
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
